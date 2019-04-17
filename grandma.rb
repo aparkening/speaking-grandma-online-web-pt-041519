2f5940e406403da8f619a6f515a7c5d80664f412
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-17 18:37:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,13 @@
 # unless you shout it (type in all capitals).
 
 def speak_to_grandma(phrase)
-  if phrase.
+  if phrase.upcase == phrase
+    puts "NO, NOT SINCE 1938!"
+  else if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else 
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 
