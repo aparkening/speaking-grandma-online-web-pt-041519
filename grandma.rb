0fcba45fe0b4d0606cf63aaaf8cb87fb1f81694d
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-17 18:35:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,6 +4,11 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
+def speak_to_grandma(phrase)
+  if phrase.
+end
+
+
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
