6250db66262219237762f5d007ba38e5df65a72b
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-17 18:41:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,11 +8,11 @@ require 'pry'
 def speak_to_grandma(phrase)
   binding.pry
   
-  if phrase.upcase == phrase
-    puts "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else 
+  elsif phrase.upcase == phrase
+    puts "NO, NOT SINCE 1938!"
+  else
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
